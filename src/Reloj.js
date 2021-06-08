daccedc9d3683852323d7ce7e7b0b2f1ebf14531
diegoejp <d.jacobpulgar@gmail.com>
2021-06-08 15:25:53 -0400

    finalizado secundero

--- src/Reloj.js
+++ src/Reloj.js
@@ -1,15 +1,17 @@
 
 
-const Reloj = (()=>{
+const Reloj = (({segundos,ms,min,hr,icono})=>{
+  
     
     return(
         <div className="container bg-dark p-3 d-flex justify-content-center">
             <span className="">Reloj</span>
+            <div className="bg-dark bg-gradient text-light text-center p-3 m-1">{icono}</div>
             <div className="bg-dark bg-gradient text-light text-center p-3 m-1">0</div>
-            <div className="bg-dark bg-gradient text-light text-center p-3 m-1">0</div>
-            <div className="bg-dark bg-gradient text-light text-center p-3 m-1">0</div>
-            <div className="bg-dark bg-gradient text-light text-center p-3 m-1">0</div>
-            <div className="bg-dark bg-gradient text-light text-center p-3 m-1">0</div>
+            <div className="bg-dark bg-gradient text-light text-center p-3 m-1">{hr}</div>
+            <div className="bg-dark bg-gradient text-light text-center p-3 m-1">{min}</div>
+            <div className="bg-dark bg-gradient text-light text-center p-3 m-1">{segundos}</div>
+            <div className="bg-dark bg-gradient text-light text-center p-3 m-1">{ms}</div>
         </div>
     )
 });
--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
-import React from "react";
 import ReactDOM from "react-dom";
 import Reloj from "./Reloj"
 import "bootstrap/dist/css/bootstrap.min.css"
-import Contador from "./ContadorBoton";
+import { useEffect, useState } from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faClock} from "@fortawesome/free-solid-svg-icons";
+
+
+
 
 let App = (()=>{
+    const [ms,setMs] = useState(0);
+    useEffect(()=>{
+        setInterval(()=>{
+        setMs(prevMs=>(prevMs+10));
+    },10);
+    },[]);
+    let milisegundo = Math.floor((ms/100)%10);
+    let minutos = Math.floor((ms/10000)%10);
+    let segundos =Math.floor((ms/1000)%10);
+    let hora = Math.floor((ms/100000)%10);
+    
+    
     return(
         <>
-            <Reloj/>
-            <Contador/>
+            
+            <Reloj ms={milisegundo} segundos={segundos} min={minutos}
+            hr={hora}
+            icono={<FontAwesomeIcon icon={faClock}/>}
+            />
         </>
     )
 });
